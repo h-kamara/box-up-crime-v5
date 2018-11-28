eaac0ed345d905db0365770044e89484e491206d
hkamara <hassan.kamara@ada.ac.uk>
2018-11-28 11:23:10 +0000

    new sticky nav bar

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,8 +1,28 @@
-var navToggle = document.querySelector('.nav-box-toggle');
-var navigationBox = document.querySelector('.nav-box');
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+var navToggle = document.querySelector('.navigation-box-toggle');
+var navigationBox = document.querySelector('.navigation-box');
 
 navToggle.addEventListener('click', toggleClass);
 
 function toggleClass(){
-  navigationBox.classList.toggle('nav-box-closed-ns')
+  navigationBox.classList.toggle('open');
+  navigationBox.classList.toggle('closed');
 }
