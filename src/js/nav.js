7ee1a79b5f78aa6f3c111a82a65856987f9d2e82
hkamara <hassan.kamara@ada.ac.uk>
2018-12-13 18:35:28 +0000

    bug fix for nav bar

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,3 +1,4 @@
+
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
@@ -9,13 +10,14 @@ var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
+  if (window.innerWidth > 850) {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
   }
 }
-
+}
 
 var navToggle = document.querySelector('.navigation-box-toggle');
 var navigationBox = document.querySelector('.navigation-box');
